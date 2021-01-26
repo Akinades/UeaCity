4a8f462c24219f6c6b365e6662ad46941c18acbb
NDFAddWork2020 <ndfdev.addwork@gmail.com>
2021-01-26 13:54:43 +0700

    SystemMakeProblem
    sss

---
+++ Assets/Scripts/NutScript/SystemMakeProblem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemMakeProblem : MonoBehaviour
+{
+     private float time = 0.0f;
+ public float interpolationPeriod = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          time += Time.deltaTime;
+ 
+     if (time >= interpolationPeriod) {
+         time = 0.0f;
+ 
+         Debug.Log("Get Problem every interpolationPeriod seconds");
+     }
+    }
+}
