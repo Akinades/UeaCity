36cb797e7fb9312a0f8e8f04966614f90ab99575
NDFAddWork2020 <ndfdev.addwork@gmail.com>
2021-01-27 21:25:48 +0700

    UI Over Building

---
+++ Assets/Scripts/NutScript/worldUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldUI : MonoBehaviour
+{
+    public Transform world;
+    public RectTransform ui;
+    public new Camera camera;
+
+    void Start()
+    {
+        camera=Camera.main;
+   
+    }
+    
+    void Update()
+    {
+       //DebugLog("PP");
+        ui.position = camera.WorldToScreenPoint(world.position);
+    }
+}
