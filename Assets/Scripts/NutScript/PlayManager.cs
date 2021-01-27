30948338823612cb9bc4ce44e641208fee946793
NDFAddWork2020 <ndfdev.addwork@gmail.com>
2021-01-27 23:40:53 +0700

    New PlayManager

---
+++ Assets/Scripts/NutScript/PlayManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    public GameObject UIfactory;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+
+                    if (hit.collider.gameObject.name == "Bank")
+                    {
+                        UIfactory.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
