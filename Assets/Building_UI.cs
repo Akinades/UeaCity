1625ea6a0ff0fd4f63c301838ee76beea2f327cb
Akinades <apisit15600@gmail.com>
2021-02-04 16:54:19 +0700

    Fix UI

---
+++ Assets/Building_UI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Building_UI : MonoBehaviour
+{
+    public Animator buildingUI;
+    public GameObject buildObject; 
+    public bool buildON = false;
+    public Button buildClick;
+    public int ClickCount = 0;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+        Animator  buildingUI = GetComponent<Animator>();
+        buildClick.onClick.AddListener(BuildingClick);
+
+      
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(buildON == true && ClickCount == 1)
+        {
+            buildObject.SetActive(true);
+            buildingUI.Play("Panel_UI");
+        }else if ( ClickCount == 2 )
+        {
+            ClickCount = 0; 
+            buildON = false;
+            buildObject.SetActive(false); 
+
+
+
+        }else
+        {
+
+        }
+       
+    }
+    void BuildingClick()
+    {
+        buildON = true;
+        ClickCount += 1;
+        buildingUI.enabled = true;
+    }
+
+
+}
